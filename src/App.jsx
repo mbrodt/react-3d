56142e5460bcce9fc069df42122f083580f8dc72
mbrodt <madsbrodt3@gmail.com>
2023-09-15 14:32:41 +0200

    feat: Init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SplineScene from "./Spline";
+import Three from "./Three";
+
+export default function App() {
+  return (
+    <div className="bg-gray-200 h-screen w-screen">
+      <Three />
+      {/* <SplineScene /> */}
+    </div>
+  );
+}
---
+++ src/Spline.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function SplineScene() {
+  return (
+    <Spline scene="https://prod.spline.design/5b5sjEn2cpIHgQBG/scene.splinecode" />
+  );
+}
